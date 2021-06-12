ea758f78c07d485f2189740ed6900a562329549a
X1turboZ <85380298+X1turboZ@users.noreply.github.com>
2021-06-12 21:48:52 +0900

    Shape Base Class

---
+++ Programming Theory Project/Assets/Scripts/shape.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shape : MonoBehaviour
+{
+	public virtual void DisplayInfo () {
+		Debug.Log ("shape info");
+	}
+}
